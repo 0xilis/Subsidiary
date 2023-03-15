565dd644e48e6aa54de3393c9997a759fd4ff644
0xilis <109871561+0xilis@users.noreply.github.com>
2023-03-15 15:42:43 -0400

    dont override superclass method

--- libsubsidiary/libsubsidiary.c
+++ libsubsidiary/libsubsidiary.c
@@ -2,12 +2,23 @@
 #include "libsubsidiary.h"
 
 //TODO: objc method hook functions actually aren't only a shim for class_replaceMethod, they actually have some more behavior, see kirb's comment https://www.reddit.com/r/jailbreak/comments/111kbp4/comment/j8hvk9l/ as well as substrate docs http://www.cydiasubstrate.com/api/c/MSHookMessageEx/
-//but for now, as wip im just gonna have it be a wrapper until i have time
 
 void SubsidiaryGenericHookMethod(Class cls, SEL name, IMP imp, IMP *orig) {
  Method hookMethod = class_getInstanceMethod(cls, name);
  if (!hookMethod) {
-  return -1;
+  printf("libsubsidiary error\n");
+  return;
  }
- *orig = class_replaceMethod(cls, name, imp, method_getTypeEncoding(hookMethod));
+ unsigned int numberOfMethods = 0;
+ Method *methods = class_copyMethodList(cls, &numberOfMethods);
+ for (Method m = methods; m < numberOfMethods; methods++) {
+  if (method_getName(m) == name) {
+   //the method is in this class so we can safely replace the method
+   *orig = class_replaceMethod(cls, name, imp, method_getTypeEncoding(hookMethod));
+   return;
+  }
+ }
+ //the method is in the superclass, add the method instead to not override superclass and get IMP of orig
+ *orig = method_getImplementation(hookMethod);
+ class_addMethod(cls, name, imp, method_getTypeEncoding(hookMethod));
 }
